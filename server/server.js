46d803235b4531e4ae5c9bafa6ae1caca8a64d2a
Roger Bolser <carydeveloper@gmail.com>
2018-06-08 10:10:18 -0400

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -107,7 +107,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
   let body = _.pick(req.body, ['email', 'password']);
 
